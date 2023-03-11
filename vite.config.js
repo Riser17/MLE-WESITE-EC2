59b7bbc29bcb5b1d03158db8b416a7d37695d72e
Riser17 <91198103+Riser17@users.noreply.github.com>
2023-03-11 18:13:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {"/api": "http://localhost:9090" },
+    proxy: {"/api": "http://3.80.94.112:9090" },
   },
   plugins: [react()],
 })
